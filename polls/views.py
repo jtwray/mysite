8e678c6edbd02565e2e452d45319cc4b8052259d
Tucker Wray <tuckerwray84@gmail.com>
2018-11-15 15:26:12 -0500

    intialized Python Django project with virtualenv on local host port 8000

---
+++ polls/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello World, You're at the polls index.")
